f80b71167ec1db2e8807aa870abc9f7b234124c4
Mateusz <mati0598@tlen.pl>
2019-12-19 23:04:40 +0100

    napis new game

--- PP-projekt.cpp
+++ PP-projekt.cpp
@@ -1,1 +1,42 @@
-abc
+#include <unistd.h>
+#include <ncurses.h>
+
+class NEW_GAME
+{
+	private:
+	int x, y;
+	
+	
+	public:
+	NEW_GAME()
+	{
+		x=(COLS-5)/2;
+		y=7;
+	}
+
+	void Print()
+	{
+		mvprintw(y,x,"NEW GAME");
+	}
+
+};
+
+
+int main()
+{
+
+	initscr();
+	noecho();
+	curs_set(0);
+
+	NEW_GAME new_game;
+	new_game.Print();
+
+	refresh();
+	sleep(5);
+
+	endwin();
+
+	return 0;
+
+}
