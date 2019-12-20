f3eed7194ce21fca367375e7f4674388868544c6
Mateusz <mati0598@tlen.pl>
2019-12-20 18:31:47 +0100

    Zdefiniowany wymiar okna z napisami

--- PP-projekt.cpp
+++ PP-projekt.cpp
@@ -1,22 +1,65 @@
 #include <unistd.h>
 #include <ncurses.h>
 
-class NEW_GAME
+WINDOW *win;
+
+class Window
+{	
+	private:
+	int height_window, width_window, startx_window, starty_window;
+	
+
+	public:
+	Window()
+	{
+		height_window=10;
+		width_window=20;
+		startx_window=30;
+		starty_window=7;
+
+	}
+
+	void Create_Window()
+	{
+		win=newwin(height_window,width_window,starty_window,startx_window);
+		refresh();
+		
+	}
+	
+	void Draw_Border()
+	{
+		box(win,0,0);
+		wrefresh(win);
+	}
+
+	
+
+};
+
+
+
+
+class MENU
 {
 	private:
-	int x, y;
+	int x1,x2,y1,y2;
+	
 	
 	
 	public:
-	NEW_GAME()
+	MENU()
 	{
-		x=(COLS-5)/2;
-		y=7;
+		x1=6;
+		x2=8;
+		y1=3;
+		y2=6;
 	}
 
 	void Print()
 	{
-		mvprintw(y,x,"NEW GAME");
+		mvwprintw(win,y1,x1,"NEW GAME");
+		mvwprintw(win,y2,x2,"QUIT");
+		wrefresh(win);
 	}
 
 };
@@ -29,12 +72,16 @@ int main()
 
 	noecho();
 	curs_set(0);
+	
+	Window W1;
+	W1.Create_Window();
+	W1.Draw_Border();
+	
+	MENU menu;
+	menu.Print();
 
-	NEW_GAME new_game;
-	new_game.Print();
 
-
-	refresh();
+	
 	sleep(5);
 
 	endwin();
